0cec39f4457bec7a397fb063ac0ca789bfa347c8
steveoh <sgourley@utah.gov>
2024-04-15 17:20:57 -0600

    feat: add a sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
-import mdx from "@astrojs/mdx";
-import tailwind from "@astrojs/tailwind";
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
 
-import react from "@astrojs/react";
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
   site: process.env.BRANCH === 'main' ? process.env.URL : process.env.DEPLOY_URL || 'https://agrc-status.netlify.app',
-  integrations: [tailwind(), mdx(), react()]
+  integrations: [tailwind(), sitemap(), mdx(), react()],
 });
