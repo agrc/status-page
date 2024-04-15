0fef7bb5f8b09bfad97125a65e775d5ad38da0f5
steveoh <sgourley@utah.gov>
2024-04-15 16:49:04 -0600

    fix: correct typescript errors

--- src/content/config.ts
+++ src/content/config.ts
@@ -5,7 +5,12 @@ const incident = defineCollection({
     title: z.string(),
     date: z.coerce.date(),
     affectedSystems: z.array(z.string()),
-    severity: z.string(),
+    severity: z.union([
+      z.literal('under-maintenance'),
+      z.literal('degraded-performance'),
+      z.literal('partial-outage'),
+      z.literal('major-outage'),
+    ]),
     resolved: z.boolean().default(false),
   }),
 });
