e9d9fa6518018dbc86a665ad5c8e42dbe6456db8
steveoh <sgourley@utah.gov>
2024-04-15 18:08:39 -0600

    chore: update collection schema

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,7 +4,18 @@ const incident = defineCollection({
   schema: z.object({
     title: z.string(),
     date: z.coerce.date(),
-    affectedSystems: z.array(z.string()),
+    affectedSystems: z.array(
+      z.union([
+        z.literal('ugrc api'),
+        z.literal('turn gps'),
+        z.literal('our website'),
+        z.literal('discover base maps and imagery'),
+        z.literal('open sgid'),
+        z.literal('sgid on arcgis'),
+        z.literal('license manager'),
+        z.literal('mapserv'),
+      ]),
+    ),
     severity: z.union([
       z.literal('under-maintenance'),
       z.literal('degraded-performance'),
