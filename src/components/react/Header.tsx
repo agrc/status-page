5a04ebfb03821ac92ecbce69ab8284cd83b4ef8d
steveoh <sgourley@utah.gov>
2025-05-14 13:39:30 -0600

    fix: replace space-* utilties from tailwind 4
    https://tailwindcss.com/docs/upgrade-guide#space-between-selector

--- src/components/react/Header.tsx
+++ src/components/react/Header.tsx
@@ -11,28 +11,30 @@ export const Header = ({ className, children }: HeaderProps) => {
     <Collapsible.Root>
       <header className={className}>
         <div className="border-b-primary-100/30 flex items-center justify-between border-b px-3 pt-1 pb-2 dark:border-black/10">
-          <div className="flex h-16 flex-1 flex-row space-x-4 divide-x divide-slate-500 sm:space-x-6">
-            <Collapsible.Trigger className="h-16 w-28">
-              <span className="sr-only">An official website of the State of Utah. Click to learn more.</span>
-              <svg
-                className="fill-primary-100 text-sm dark:fill-white"
-                style={{ fontFamily: 'SourceSansPro-Regular,Source Sans Pro,Arial,Helvetica,sans-serif' }}
-                viewBox="0 0 105.9496 47.6226"
-                role="img"
-                aria-hidden="true"
-              >
-                <g>
+          <div className="flex h-16 divide-x divide-slate-500">
+            <div className="pr-4 sm:pr-6">
+              <Collapsible.Trigger className="h-16 w-28">
+                <span className="sr-only">An official website of the State of Utah. Click to learn more.</span>
+                <svg
+                  className="fill-primary-100 text-sm dark:fill-white"
+                  style={{ fontFamily: 'SourceSansPro-Regular,Source Sans Pro,Arial,Helvetica,sans-serif' }}
+                  viewBox="0 0 105.9496 47.6226"
+                  role="img"
+                  aria-hidden="true"
+                >
                   <g>
-                    <path d="M12.2714,30.0877c-4.1489,0-7.2318-1.2037-9.2489-3.611C1.0055,24.0693-.002,20.334,0,15.2709V0H7.8175V16.1806c0,2.6363,.356,4.4923,1.0679,5.5679,.7613,1.1018,2.0506,1.7162,3.3859,1.6134,1.3465,.0953,2.6458-.5157,3.4313-1.6134,.7422-1.0756,1.1133-2.9316,1.1133-5.5679V0h7.5448V15.2709c0,5.0601-.9847,8.7946-2.9541,11.2035-1.9694,2.4089-5.0145,3.6133-9.1352,3.6133Zm24.0887-.5463V6.5444h-7.8175V0h23.4526V6.5444h-7.8175V29.5414h-7.8175Zm25.8151-14.362l-.5002,2.0452h5.455l-.5002-2.0452c-.3637-1.4239-.7273-2.9693-1.091-4.636-.3637-1.6667-.7261-3.242-1.0871-4.7259h-.1821c-.3334,1.5151-.6743,3.0983-1.0226,4.7497s-.7053,3.189-1.071,4.6129l-.0008-.0008Zm-11.3617,14.362L59.8127,0h9.4502l9.0023,29.5414h-8.2724l-1.4544-6.2709h-8.2716l-1.4544,6.2709h-7.9988Zm30.2713,0V0h7.8175V10.9991h8.8171V0h7.8175V29.5414h-7.8175v-11.7251h-8.8194v11.7251h-7.8152Z"></path>
+                    <g>
+                      <path d="M12.2714,30.0877c-4.1489,0-7.2318-1.2037-9.2489-3.611C1.0055,24.0693-.002,20.334,0,15.2709V0H7.8175V16.1806c0,2.6363,.356,4.4923,1.0679,5.5679,.7613,1.1018,2.0506,1.7162,3.3859,1.6134,1.3465,.0953,2.6458-.5157,3.4313-1.6134,.7422-1.0756,1.1133-2.9316,1.1133-5.5679V0h7.5448V15.2709c0,5.0601-.9847,8.7946-2.9541,11.2035-1.9694,2.4089-5.0145,3.6133-9.1352,3.6133Zm24.0887-.5463V6.5444h-7.8175V0h23.4526V6.5444h-7.8175V29.5414h-7.8175Zm25.8151-14.362l-.5002,2.0452h5.455l-.5002-2.0452c-.3637-1.4239-.7273-2.9693-1.091-4.636-.3637-1.6667-.7261-3.242-1.0871-4.7259h-.1821c-.3334,1.5151-.6743,3.0983-1.0226,4.7497s-.7053,3.189-1.071,4.6129l-.0008-.0008Zm-11.3617,14.362L59.8127,0h9.4502l9.0023,29.5414h-8.2724l-1.4544-6.2709h-8.2716l-1.4544,6.2709h-7.9988Zm30.2713,0V0h7.8175V10.9991h8.8171V0h7.8175V29.5414h-7.8175v-11.7251h-8.8194v11.7251h-7.8152Z"></path>
+                    </g>
+                    <text transform="translate(.0419 43.5205)" aria-hidden="true">
+                      <tspan x="0" y="0">
+                        An official website
+                      </tspan>
+                    </text>
                   </g>
-                  <text transform="translate(.0419 43.5205)" aria-hidden="true">
-                    <tspan x="0" y="0">
-                      An official website
-                    </tspan>
-                  </text>
-                </g>
-              </svg>
-            </Collapsible.Trigger>
+                </svg>
+              </Collapsible.Trigger>
+            </div>
             <div className="flex flex-1 justify-start pl-3 sm:pl-5">{children}</div>
           </div>
         </div>
