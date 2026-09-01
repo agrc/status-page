e8d221a6f9cb54681cd794896ed73a5ba956e7a4
stdavis <stdavis@utah.gov>
2026-09-01 09:57:18 -0600

    deps: major bump NPM deps 🌲

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 import mdx from '@astrojs/mdx';
+import { unified } from '@astrojs/markdown-remark';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
@@ -7,6 +8,7 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   site: process.env.BRANCH === 'main' ? process.env.URL : process.env.DEPLOY_URL || 'https://agrc-status.netlify.app',
+  markdown: { processor: unified() },
   integrations: [sitemap(), mdx(), react()],
   vite: { plugins: [tailwindcss()] },
 });
